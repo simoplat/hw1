49f90f08de8e8aea00f0524a26ade73ad9e2d6e9
simoplat <150624084+simoplat@users.noreply.github.com>
2025-05-22 12:38:14 +0200

    update

--- signup.php
+++ signup.php
@@ -83,14 +83,14 @@
         <script src='signup.js' defer></script>
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/png" href="favicon.png">
         <meta charset="utf-8">
 
-        <title>Iscriviti - Musity</title>
+        <title>Iscrizione</title>
     </head>
     <body>
         <div id="logo">
-            My site
+            <img src="media/YouTube-logo.png">
+            <h1>Inserisci i tuoi dati</h1>
         </div>
         <main>
         <section class="main_left">
@@ -103,37 +103,37 @@
                         <!-- Se il submit non va a buon fine, il server reindirizza su questa stessa pagina, quindi va ricaricata con 
                             i valori precedentemente inseriti -->
                         <input type='text' name='name' <?php if(isset($_POST["name"])){echo "value=".$_POST["name"];} ?> >
-                        <div><img src="close.svg"/><span>Devi inserire il tuo nome</span></div>
+                        <div><img src="media/close.svg"/><span>Devi inserire il tuo nome</span></div>
                     </div>
                     <div class="surname">
                         <label for='surname'>Cognome</label>
                         <input type='text' name='surname' <?php if(isset($_POST["surname"])){echo "value=".$_POST["surname"];} ?> >
-                        <div><img src="close.svg"/><span>Devi inserire il tuo cognome</span></div>
+                        <div><img src="media/close.svg"/><span>Devi inserire il tuo cognome</span></div>
                     </div>
                 </div>
                 <div class="username">
                     <label for='username'>Nome utente</label>
                     <input type='text' name='username' <?php if(isset($_POST["username"])){echo "value=".$_POST["username"];} ?>>
-                    <div><img src="close.svg"/><span>Nome utente non disponibile</span></div>
+                    <div><img src="media/close.svg"/><span>Nome utente non disponibile</span></div>
                 </div>
                 <div class="email">
                     <label for='email'>Email</label>
                     <input type='text' name='email' <?php if(isset($_POST["email"])){echo "value=".$_POST["email"];} ?>>
-                    <div><img src="close.svg"/><span>Indirizzo email non valido</span></div>
+                    <div><img src="media/close.svg"/><span>Indirizzo email non valido</span></div>
                 </div>
                 <div class="password">
                     <label for='password'>Password</label>
                     <input type='password' name='password' <?php if(isset($_POST["password"])){echo "value=".$_POST["password"];} ?>>
-                    <div><img src="close.svg"/><span>Inserisci almeno 8 caratteri</span></div>
+                    <div><img src="media/close.svg"/><span>Inserisci almeno 8 caratteri</span></div>
                 </div>
                 <div class="confirm_password">
                     <label for='confirm_password'>Conferma Password</label>
                     <input type='password' name='confirm_password' <?php if(isset($_POST["confirm_password"])){echo "value=".$_POST["confirm_password"];} ?>>
-                    <div><img src="close.svg"/><span>Le password non coincidono</span></div>
+                    <div><img src="media/close.svg"/><span>Le password non coincidono</span></div>
                 </div>
                 <div class="allow"> 
                     <input type='checkbox' name='allow' value="1" <?php if(isset($_POST["allow"])){echo $_POST["allow"] ? "checked" : "";} ?>>
-                    <label for='allow'>Accetto i termini e condizioni d'uso di Musity.</label>
+                    <label for='allow'>Accetto i termini e condizioni d'uso.</label>
                 </div>
                 <?php if(isset($error)) {
                     foreach($error as $err) {
@@ -144,7 +144,7 @@
                     <input type='submit' value="Registrati" id="submit">
                 </div>
             </form>
-            <div class="signup">Hai un account? <a href="login.php">Accedi</a>
+            <div class="signup">Hai un account? <a href="index.php">Accedi</a>
         </section>
         </main>
     </body>
